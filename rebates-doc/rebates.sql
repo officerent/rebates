809f258a24cb42e89563b3a574c2b138c2da8f0a
xuximing <xuxim99@hotmail.com>
2015-12-28 10:33:57 +0800

    db created

--- rebates-doc/rebates.sql
+++ rebates-doc/rebates.sql
@@ -1,96 +1,119 @@
 
-drop database soho3q_cms;
-create database if not exists soho3q_cms default charset utf8 collate utf8_general_ci;
+drop database rebates;
+create database if not exists rebates default charset utf8 collate utf8_general_ci;
 
-use soho3q_cms;
+use rebates;
 
-CREATE TABLE `cms_channel` (
-  `channel_id` bigint NOT NULL AUTO_INCREMENT COMMENT '频道ID',
-  `name` varchar(50) COMMENT '频道名称',
-  `description` varchar(2048) COMMENT '频道描述',
-  `status` tinyint comment '状态:0=正常，1=删除',
-  `css_file` varchar(2048) COMMENT 'css文件地址',
-  `creator` varchar(50) COMMENT '创建人',
+CREATE TABLE `rebates_user` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `name` varchar(50) COMMENT '用户名',
+  `password` varchar(2048) COMMENT '用户密码，md5加密以后的值',
+  `status` tinyint comment '状态:0=正常，1=冻结，2=删除',
+  `is_admin` tinyint comment '是否管理员:0=不是，1=是',
+  `last_frozen_time` datetime comment '最后一次冻结时间',
   `create_time` datetime comment '创建时间',
-  `updater` varchar(50) COMMENT '修改人',
   `last_update_time` datetime comment '最后一次更新时间',
-  PRIMARY KEY (`channel_id`),
+  PRIMARY KEY (`user_id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='频道表';
+) AUTO_INCREMENT = 10000 ENGINE=InnoDB DEFAULT CHARSET=utf8  COMMENT='用户表';
 
-CREATE TABLE `cms_tag` (
-  `tag_id` bigint NOT NULL AUTO_INCREMENT COMMENT '标签ID',
-  `channel_name` varchar(50) COMMENT '频道名称',
-  `name` varchar(50) COMMENT '标签名称',
-  `color` varchar(50) COMMENT '颜色',
-  `creator` varchar(50) COMMENT '创建人',
+CREATE TABLE `question_answer` (
+  `qa_id` bigint NOT NULL AUTO_INCREMENT COMMENT 'QA ID',
+  `user_id` bigint NOT NULL COMMENT '用户ID',
+  `root_qa_id` bigint COMMENT 'root QA ID，最上一层',
+  `parent_qa_id` bigint COMMENT '上一层 QA ID',
+  `content` varchar(1024) COMMENT '内容',
   `create_time` datetime comment '创建时间',
-  `updater` varchar(50) COMMENT '修改人',
   `last_update_time` datetime comment '最后一次更新时间',
-  PRIMARY KEY (`tag_id`),
-  KEY `channel_name` (`channel_name`),
-  KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='标签表';
+  PRIMARY KEY (`qa_id`),
+  KEY `root_qa_id` (`root_qa_id`),
+  KEY `user_id` (`user_id`)
+) AUTO_INCREMENT = 10000 ENGINE=InnoDB DEFAULT CHARSET=utf8  COMMENT='用户问答表';
 
-CREATE TABLE `cms_key` (
-  `key_id` bigint NOT NULL AUTO_INCREMENT COMMENT 'key ID',
-  `channel_name` varchar(50) COMMENT '频道名称',
-  `tag_id` bigint COMMENT 'tag ID',
-  `cms_key` varchar(250) NOT NULL COMMENT 'key',
-  `name` varchar(50) NOT NULL COMMENT 'key的名字',
-  `description` varchar(2048) NOT NULL COMMENT 'key的描述',
-  `key_type` varchar(50) COMMENT 'key的类型:IMAGE,JSON,HTML,INT,STRING,BOOLEAN',
-  `enabled` tinyint comment 'key状态:0=key未生效，1=key已生效',
-  `creator` varchar(50) COMMENT '创建人',
+#token可以不用存数据库，直接放缓存#
+CREATE TABLE `user_token` (
+  `token_id` bigint NOT NULL AUTO_INCREMENT COMMENT '用户tokenID',
+  `user_id` bigint NOT NULL COMMENT '用户ID',
+  `token` varchar(2048) COMMENT 'token value，md5(user_id + 当前时间)',
   `create_time` datetime comment '创建时间',
-  `updater` varchar(50) COMMENT '修改人',
-  `last_update_time` datetime comment '最后一次更新时间',
-  PRIMARY KEY (`key_id`),
-  KEY `channel_tag` (`channel_name`,`tag_id`),
-  UNIQUE KEY `cms_key` (`channel_name`,`cms_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='key表';
+  PRIMARY KEY (`channel_id`),
+  UNIQUE KEY `token` (`token`)
+) AUTO_INCREMENT = 10000 ENGINE=InnoDB DEFAULT CHARSET=utf8  COMMENT='用户token表';
 
-CREATE TABLE `cms_value` (
-  `value_id` bigint NOT NULL AUTO_INCREMENT COMMENT 'value ID',
-  `channel_name` varchar(50) COMMENT '频道名称',
-  `cms_key` varchar(250) NOT NULL COMMENT 'key',
-  `language` varchar(50) NOT NULL COMMENT '语言:zh_CN或者en_US, *表示通配符',
-  `client` varchar(50) NOT NULL COMMENT '客户端Web，H5、APP、WeChat, *表示通配符',
-  `os` varchar(50) NOT NULL COMMENT '操作系统:IOS、Andriod, *表示通配符', 
-  `business_id` varchar(100) COMMENT '业务ID',
-  `cms_value` varchar(10000) NOT NULL COMMENT 'value',
-  `enabled` tinyint comment 'value状态:0=value未生效，1=value已生效',
-  `creator` varchar(50) COMMENT '创建人',
+CREATE TABLE `sales_people` (
+  `sales_id` bigint NOT NULL AUTO_INCREMENT COMMENT 'sales ID',
+  `sales_num` int(11) NOT NULL COMMENT '销售的编号，1,2,3,4等，下单按此编号随机挑选一个销售',
+  `user_name` varchar(50) NOT NULL COMMENT '该销售在soho3q中的登录名',
+  `user_password` varchar(50) NOT NULL COMMENT '该销售在soho3q中的登录密码明文',  
   `create_time` datetime comment '创建时间',
-  `updater` varchar(50) COMMENT '修改人',
   `last_update_time` datetime comment '最后一次更新时间',
-  `description` varchar(300) comment '策略描述',
-  PRIMARY KEY (`value_id`),
-  KEY `cms_key` (`channel_name`,`cms_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='value表';
+  PRIMARY KEY (`sales_id`),
+  UNIQUE KEY `token` (`sales_num`)
+) AUTO_INCREMENT = 10000 ENGINE=InnoDB DEFAULT CHARSET=utf8  COMMENT='soho3q销售表';
 
-CREATE TABLE `cms_value_history` (
-  `value_history_id` bigint NOT NULL AUTO_INCREMENT COMMENT 'value history ID',
-  `value_id` bigint NOT NULL COMMENT 'value ID',
-  `channel_name` varchar(50) COMMENT '频道名称',
-  `cms_key` varchar(250) NOT NULL COMMENT 'key',
-  `language` varchar(50) NOT NULL COMMENT '语言:zh_CN或者en_US, *表示通配符',
-  `client` varchar(50) NOT NULL COMMENT '客户端Web，H5、APP、WeChat, *表示通配符',
-  `os` varchar(50) NOT NULL COMMENT '操作系统:IOS、Andriod, *表示通配符', 
-  `business_id` varchar(100) COMMENT '业务ID',
-  `cms_value` varchar(10000) NOT NULL COMMENT 'value',
-  `enabled` tinyint comment 'value状态:0=value未生效，1=value已生效',
-  `creator` varchar(50) COMMENT '创建人',
+CREATE TABLE `rebates_order` (
+  `order_id` bigint NOT NULL AUTO_INCREMENT COMMENT '订单ID',
+  `user_id` bigint NOT NULL COMMENT '用户ID',
+  `sales_id` bigint COMMENT '销售ID,使用该销售向soho3q下单',
+  `soho3q_order_id` bigint COMMENT 'soho3q需求单',
+  #`status` tinyint comment '状态:0=用户下单，1=已向soho3q下单',
+  `customer_mobile` varchar(50) COMMENT '客户手机',
+  `customer_name` varchar(50) COMMENT '客户姓名',
+  `customer_company` varchar(50) COMMENT '客户公司',
+  `customer_alipay` varchar(50) COMMENT '客户支付宝帐号',
+  `porject_id` varchar(50) COMMENT '项目id',
+  `porject_name` varchar(50) COMMENT '项目名称',
+  `checkin_date` date comment '入住日期',
+  `checkout_date` date comment '退租日期',
+  `period_month` int(11) COMMENT '租期,月',
+  `period_week` int(11) COMMENT '租期,周',
+  `lease_amount` int(11) COMMENT '租金金额，单位分',
+  `deposit_amount` int(11) COMMENT '押金金额，单位分',
   `create_time` datetime comment '创建时间',
-  `updater` varchar(50) COMMENT '修改人',
   `last_update_time` datetime comment '最后一次更新时间',
-  `description` varchar(300) comment '策略描述',
-  PRIMARY KEY (`value_history_id`),
-  KEY `cms_key` (`channel_name`,`cms_key`),
-  KEY `value_id` (`value_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='value历史表';
+  PRIMARY KEY (`order_id`),
+  KEY `user_id` (`user_id`),
+  UNIQUE KEY `soho3q_order_id` (`soho3q_order_id`),
+  KEY `sales_id` (`sales_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
+
+CREATE TABLE `rebates_order_item` (
+  `order_item_id` bigint NOT NULL AUTO_INCREMENT COMMENT '订单明细ID',
+  `order_id` bigint NOT NULL COMMENT 'order ID',
+  `porject_id` varchar(50) COMMENT '项目id',
+  `original_price` int(11) COMMENT '产品租金原单价，单位分',
+  `final_price` int(11) COMMENT '产品折后租金单价，单位分',
+  `deposit_price` int(11) COMMENT '押金金额，单位分',
+  `product_type` varchar(50) COMMENT '产品大类',
+  `product_sub_type` varchar(50) COMMENT '产品小类',
+  `book_num` int(11) COMMENT '产品数量',
+  `total_price` int(11) COMMENT '该品总价，单位分',
+  `create_time` datetime comment '创建时间',
+  `last_update_time` datetime comment '最后一次更新时间',
+  PRIMARY KEY (`order_id`),
+  KEY `user_id` (`user_id`),
+  UNIQUE KEY `soho3q_order_id` (`soho3q_order_id`),
+  KEY `sales_id` (`sales_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单明细表';
+
+CREATE TABLE `rebates_bonus` (
+  `bonus_id` bigint NOT NULL AUTO_INCREMENT COMMENT 'key ID',
+  `order_id` bigint NOT NULL COMMENT '订单ID',
+  `sales_id` bigint COMMENT '销售ID',
+  `soho3q_order_id` bigint COMMENT 'soho3q需求单id',
+  `soho3q_order_num` bigint COMMENT 'soho3q订单编号',
+  `lease_amount` int(11) COMMENT '支付租金金额，单位分',
+  `bonus_before_tax` int(11) COMMENT 'soho3q佣金税前金额，单位分',
+  `bonus_tax` int(11) COMMENT 'soho3q佣金税金额，单位分',
+  `bonus_after_tax` int(11) COMMENT 'soho3q佣金税后金额，单位分', 
+  `rebates_amount` int(11) COMMENT '给客户的返利金额，单位分',
+  `status` tinyint COMMENT '0=需求单待确认，1=需求单已确认，2=客户已支付订单，3=销售已收到佣金，4=客户已经收到返利',
+  `create_time` datetime comment '创建时间',
+  `last_update_time` datetime comment '最后一次更新时间',
+  `updater` varchar(50) COMMENT '修改人',
+  PRIMARY KEY (`bonus_id`),
+  KEY `order_id` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='佣金表';
 
 
-## cms_value 、 cms_value_history分别添加 description 字段
-Alter table cms_value_history change cms_value cms_value varchar(10000) NOT NULL COMMENT 'value';
 
