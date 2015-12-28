0579c079fb40bdd30e900319e0b1ab7023ee3091
xuximing <xuxim99@hotmail.com>
2015-12-28 19:02:23 +0800

    commit

---
+++ rebates-model/src/main/java/com/office/rebates/model/common/Constants.java
@@ -0,0 +1,8 @@
+package com.office.rebates.model.common;
+
+public class Constants {
+	public static String LANG_CN = "zh_CN";
+	public static String LANG_EN = "en_US";
+
+		
+}
---
+++ rebates-web/src/main/java/com/office/rebates/controller/RouteKey.java
@@ -0,0 +1,11 @@
+package com.office.rebates.controller;
+
+/**
+ * Created by xuximing on 15/12/17.
+ */
+public class RouteKey {
+
+
+    
+    
+}
