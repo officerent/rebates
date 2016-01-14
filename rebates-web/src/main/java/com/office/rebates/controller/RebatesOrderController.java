ea31866bab9b1a63a2bc00dccdc7de65c9cb308d
xuximing <xuxim99@hotmail.com>
2016-01-14 18:40:31 +0800

    commit

--- rebates-web/src/main/java/com/office/rebates/controller/RebatesOrderController.java
+++ rebates-web/src/main/java/com/office/rebates/controller/RebatesOrderController.java
@@ -24,7 +24,7 @@ import com.office.rebates.service.UserService;
 
 
 @Controller
-@RequestMapping(RouteKey.REBATES_ORDER)
+@RequestMapping(RouteKey.PREFIX_AJAX+"/"+RouteKey.REBATES_ORDER)
 public class RebatesOrderController {
     @Autowired
     private UserService userService;
--- rebates-web/src/main/java/com/office/rebates/controller/RouteKey.java
+++ rebates-web/src/main/java/com/office/rebates/controller/RouteKey.java
@@ -4,6 +4,8 @@ package com.office.rebates.controller;
  * Created by xuximing on 15/12/31.
  */
 public class RouteKey {
+    public static final String PREFIX_AJAX = "/ajax";
+    
     public static final String REBATES_ORDER = "/rebates_order";
     public static final String CREATE_ORDER = "/create";
     
