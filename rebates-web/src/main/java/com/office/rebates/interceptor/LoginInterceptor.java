e0306e9ee1a16a99b61819c6a32a17819781c1a2
liyongfeng <15301392163@163.com>
2016-04-27 18:44:18 +0800

    回滚之前状态

--- rebates-web/src/main/java/com/office/rebates/interceptor/LoginInterceptor.java
+++ rebates-web/src/main/java/com/office/rebates/interceptor/LoginInterceptor.java
@@ -1,16 +1,10 @@
 package com.office.rebates.interceptor;
 
-import com.alibaba.fastjson.JSON;
-import com.office.rebates.model.UserInfo;
-import com.office.rebates.model.common.Messages;
-import com.office.rebates.util.LoginUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,8 +21,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) throws Exception {
-        boolean flag = LoginUtils.getLoginStatus(request);
-        return flag;
+        return false;
     }
 
     @Override
