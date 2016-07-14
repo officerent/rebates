cecf2a1bcdb239d81b6af44bafad761fbc0157be
liyongfeng <15301392163@163.com>
2016-07-14 15:37:21 +0800

    添加问题控制器

---
+++ rebates-web/src/main/java/com/office/rebates/controller/web/WebQuestionController.java
@@ -0,0 +1,32 @@
+package com.office.rebates.controller.web;
+
+import com.office.rebates.controller.RouteKey;
+import com.office.rebates.service.web.WebQuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 问题控制器
+ * Created by liyongfeng on 16/7/14.
+ */
+@Controller
+@RequestMapping(RouteKey.WEB_QUESTION)
+public class WebQuestionController {
+
+    /**
+     * 问题服务
+     */
+    @Autowired
+    private WebQuestionService webQuestionService;
+
+    /**
+     * 提示页
+     */
+    @RequestMapping(RouteKey.TIP)
+    public void tip(String manager,String url,Model model){
+        model.addAttribute("url",url);
+        model.addAttribute("manager",manager);
+    }
+}
